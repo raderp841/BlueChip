9103dbcf53c733965193d264814f691b3a1b3441
Peter Rader <raderp841@gmail.com>
2018-04-07 04:27:05 -0400

    work on cart jquery

---
+++ BlueChip/BlueChip/Js/Cart.js
@@ -0,0 +1,45 @@
+﻿var cartList = $("#cart-list");
+var price;
+var totalPrice;
+
+$(document).ready(function () {
+    cartList.hide();
+    $('.deleteItem').click(function () {
+        $(this).parent().remove();
+        getCount();
+    });
+});
+
+function showCart() {
+    cartList.show();
+    $("#showCart").hide();
+};
+
+function deleteItem(productId, userId, price) {
+    console.log(productId);
+    console.log(userId);
+    getTotalPrice();
+    console.log(totalPrice);
+    changeSub(price);
+    $("#total").text(totalPrice);
+    console.log(price);
+};
+
+function getCount() {
+    var count = $(".deleteItem").length;
+    var countText = $(".cartCount");
+    countText.text("You have " + count + " items");
+};
+
+function changeSub(price) {
+    totalPrice -= price;
+};
+
+function getTotalPrice() {
+    totalPrice = $("#total").html();
+    console.log(totalPrice);
+}
+
+
+
+
